f60f0ead1333d4138bcd99612854e372d2fe16b9
xrw <373698180@qq.com>
2014-08-14 17:04:55 +0800

    update

--- src/com/action/AnalyseAction.java
+++ src/com/action/AnalyseAction.java
@@ -34,33 +34,33 @@ public class AnalyseAction
         super();
     }
     
-    
     // 获取显示excel 的信息
     public String getDetail(HttpServletRequest request, HttpServletResponse response)
     {
-        //获得第几张sheet
+        // 获得第几张sheet
         int sheetId = Integer.parseInt(request.getParameter("sheetId"));
-        String fileName="";
+        String fileName = "";
         try
         {
             // 获得文件名
             fileName = new String((request.getParameter("fileName")).getBytes("ISO-8859-1"), "UTF-8");
-            //获得文件对象
+            // 获得文件对象
             File file = new File(request.getSession().getServletContext().getRealPath("/Compare/" + fileName));
-
-            //调用方法获得要显示的数据
-            String data = readExcelDetail(sheetId,file,fileName);
-            //设置输出编码格式是UTF-8 防止乱码
+            
+            // 调用方法获得要显示的数据
+            String data = readExcelDetail(sheetId, file, fileName, null, null);
+            // 设置输出编码格式是UTF-8 防止乱码
             response.setCharacterEncoding("UTF-8");
             
             PrintWriter out = response.getWriter();
-               
+            
             out.println(data);
             out.flush();
             out.close();
-  
+            
         }
-        catch (IOException e) {
+        catch (IOException e)
+        {
             e.printStackTrace();
         }
         catch (Exception e)
@@ -68,13 +68,46 @@ public class AnalyseAction
             e.printStackTrace();
         }
         return "ajax";
-
+        
     }
     
-    private String readExcelDetail(int sheetId, File file,String fileName)
+    public String getDataByDate(HttpServletRequest request, HttpServletResponse response)
+    {
+        String fileName = "";
+        try
+        {
+            fileName = new String((request.getParameter("fileName")).getBytes("ISO-8859-1"), "UTF-8");
+            int index = Integer.parseInt(request.getParameter("index"));
+            String start = request.getParameter("start");
+            String end = request.getParameter("end");
+            File file = new File(request.getSession().getServletContext().getRealPath("/Compare/" + fileName));
+            String data = readExcelDetail(index, file, fileName, start, end);
+            response.setCharacterEncoding("UTF-8");
+            
+            PrintWriter out = response.getWriter();
+            
+            out.println(data);
+            out.flush();
+            out.close();
+            
+            
+        }
+        catch (UnsupportedEncodingException e)
+        {
+            e.printStackTrace();
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+        
+        return "ajax";
+    }
+    
+    private String readExcelDetail(int sheetId, File file, String fileName, String start, String end)
     {
         StringBuffer sb = new StringBuffer();
-        //定义表头
+        boolean rightData = false;
+        // 定义表头
         sb.append("<table border='1'  cellpadding='0' cellspacing='0'>");
         try
         {
@@ -84,74 +117,126 @@ public class AnalyseAction
             {
                 isE2007 = true;
             }
-                InputStream imStream = new FileInputStream(file);
-                Workbook wb = null;
-                // 根据文件格式（2003或者2007）来初始化
-                if (isE2007)
+            InputStream imStream = new FileInputStream(file);
+            Workbook wb = null;
+            // 根据文件格式（2003或者2007）来初始化
+            if (isE2007)
+            {
+                wb = new XSSFWorkbook(imStream);
+            }
+            else
+            {
+                wb = new HSSFWorkbook(imStream);
+            }
+            // 获取某一个sheet
+            Sheet sheet = wb.getSheetAt(sheetId);
+            // 获取表单迭代器
+            Iterator<Row> rows = sheet.rowIterator();
+            int i = 0;
+            while (rows.hasNext())
+            {
+                // 如果是表头设置表头格式
+                if (i == 0)
                 {
-                    wb = new XSSFWorkbook(imStream);
+                    sb.append("<tr bgcolor='#6699FF'>");
                 }
                 else
                 {
-                    wb = new HSSFWorkbook(imStream);
-                }
-                // 获取某一个sheet
-                Sheet sheet = wb.getSheetAt(sheetId);
-                // 获取表单迭代器
-                Iterator<Row> rows = sheet.rowIterator();
-                int i=0;
-                while (rows.hasNext())
-                {
-                    // 如果是表头设置表头格式
-                    if(i==0)
+                    // 奇数偶数行分别不同样式
+                    if (i % 2 == 0)
                     {
-                        sb.append("<tr bgcolor='#6699FF'>");
+                        sb.append("<tr bgcolor='EEF1F5'>");
                     }
                     else
                     {
-                        //奇数偶数行分别不同样式
-                        if(i%2==0)
-                        {
-                            sb.append("<tr bgcolor='EEF1F5'>");
-                        }
-                        else
-                        {
-                            sb.append("<tr>");
-                        }
-                             
+                        sb.append("<tr>");
                     }
-                    // 获得行数据
-                    Row row = rows.next();
-                    // System.out.println("Row #" + row.getRowNum()); // 获得行号从0开始
                     
-                    // 定义map 存放数值
-                    Iterator<Cell> cells = row.cellIterator(); // 获得第一行的迭代器
-                    while (cells.hasNext())
+                }
+                // 获得行数据
+                Row row = rows.next();
+                // System.out.println("Row #" + row.getRowNum()); // 获得行号从0开始
+                
+                // 定义map 存放数值
+                Iterator<Cell> cells = row.cellIterator(); // 获得第一行的迭代器
+
+                while (cells.hasNext())
+                {
+                    Cell cell = cells.next();
+                    // 如果是标题那么都剧中使用ListTitle 样式
+                    if (i == 0)
                     {
-                        Cell cell = cells.next();
-                        // 如果是标题那么都剧中使用ListTitle 样式
-                        if( i == 0)
+                        sb.append("<td width align='center' class='ListTitle' nowrap>");
+                        switch (cell.getCellType())
                         {
-                           sb.append("<td width align='center' class='ListTitle' nowrap>");
-                           switch (cell.getCellType())
-                           {
-                               case HSSFCell.CELL_TYPE_STRING:
-                                   sb.append(cell.getStringCellValue());
-                                   break;
-                               case HSSFCell.CELL_TYPE_NUMERIC:
-                                   sb.append(String.valueOf(cell.getNumericCellValue()));
-                                   break;
-                               case HSSFCell.CELL_TYPE_BOOLEAN:
-                                   sb.append(String.valueOf(cell.getBooleanCellValue()));
-                                   break;
-                               case HSSFCell.CELL_TYPE_BLANK:
-                                   sb.append("empty");
-                           }
-                           sb.append("</td>");
+                            case HSSFCell.CELL_TYPE_STRING:
+                                sb.append(cell.getStringCellValue());
+                                break;
+                            case HSSFCell.CELL_TYPE_NUMERIC:
+                                sb.append(String.valueOf(cell.getNumericCellValue()));
+                                break;
+                            case HSSFCell.CELL_TYPE_BOOLEAN:
+                                sb.append(String.valueOf(cell.getBooleanCellValue()));
+                                break;
+                            case HSSFCell.CELL_TYPE_BLANK:
+                                sb.append("empty");
                         }
-                        else
+                        sb.append("</td>");
+                    }
+                    
+                    else if (start== null || end ==null)
+                    {
+                       
+                        sb.append("<td width='' align='left' class='textS2' nowrap>");
+                        switch (cell.getCellType())
                         {
-                            sb.append("<td width='' align='left' class='textS2' nowrap>");
+                            case HSSFCell.CELL_TYPE_STRING:
+                                sb.append(cell.getStringCellValue());
+                                break;
+                            case HSSFCell.CELL_TYPE_NUMERIC:
+                                sb.append(String.valueOf(cell.getNumericCellValue()));
+                                break;
+                            case HSSFCell.CELL_TYPE_BOOLEAN:
+                                sb.append(String.valueOf(cell.getBooleanCellValue()));
+                                break;
+                            case HSSFCell.CELL_TYPE_BLANK:
+                                sb.append("&nbsp;");
+                        }
+                        sb.append("</td>");
+                    }
+                    // 需要比较时间
+                    else
+                    {
+                        Cell dataecell = row.getCell(8);
+                        String value = "";
+                        int date=0;
+                        int startDate = Integer.parseInt(start.replaceAll("-",""));
+                        int endDate = Integer.parseInt(end.replaceAll("-",""));
+                            
+                        switch (dataecell.getCellType())
+                        {
+                            case HSSFCell.CELL_TYPE_STRING:
+                                value = dataecell.getStringCellValue();
+                                break;
+                            case HSSFCell.CELL_TYPE_NUMERIC:
+                                value = String.valueOf(dataecell.getNumericCellValue());
+                                break;
+                            case HSSFCell.CELL_TYPE_BOOLEAN:
+                                value = String.valueOf(dataecell.getBooleanCellValue());
+                                break;
+                            case HSSFCell.CELL_TYPE_BLANK:
+                                value = "&nbsp;";
+                         }
+                        if(value=="&nbsp;")
+                        {
+                            break;
+                        }
+                        date = Integer.parseInt(value.substring(1, 9));
+                        if( startDate<=date && date<=endDate)
+                        {                         
+                        
+                        sb.append("<td width='' align='left' class='textS2' nowrap>");
+
                             switch (cell.getCellType())
                             {
                                 case HSSFCell.CELL_TYPE_STRING:
@@ -165,47 +250,51 @@ public class AnalyseAction
                                     break;
                                 case HSSFCell.CELL_TYPE_BLANK:
                                     sb.append("&nbsp;");
-                            }  
+                            }   
                             sb.append("</td>");
                         }
+
+
                     }
-                    i++;
-                    sb.append("</tr>");
                 }
-                //表尾部追加 表头
-                sb.append("<tr bgcolor='#6699FF'> ");
-                Row row = sheet.getRow(0);
-                Iterator<Cell> cells = row.cellIterator();
-                while (cells.hasNext())
+                i++;
+                sb.append("</tr>");
+            }
+            // 表尾部追加 表头
+            sb.append("<tr bgcolor='#6699FF'> ");
+            Row row = sheet.getRow(0);
+            Iterator<Cell> cells = row.cellIterator();
+            while (cells.hasNext())
+            {
+                Cell cell = cells.next();
+                // 如果是标题那么都剧中使用ListTitle 样式
+                sb.append("<td width align='center' class='ListTitle' nowrap>");
+                switch (cell.getCellType())
                 {
-                    Cell cell = cells.next();
-                    // 如果是标题那么都剧中使用ListTitle 样式
-                    sb.append("<td width align='center' class='ListTitle' nowrap>");
-                     switch (cell.getCellType())
-                       {
-                           case HSSFCell.CELL_TYPE_STRING:
-                               sb.append(cell.getStringCellValue());
-                               break;
-                           case HSSFCell.CELL_TYPE_NUMERIC:
-                               sb.append(String.valueOf(cell.getNumericCellValue()));
-                               break;
-                           case HSSFCell.CELL_TYPE_BOOLEAN:
-                               sb.append(String.valueOf(cell.getBooleanCellValue()));
-                               break;
-                           case HSSFCell.CELL_TYPE_BLANK:
-                               sb.append("--");
-                       }
-                       sb.append("</td>");
-                    }
-                sb.append("</te></table>");
+                    case HSSFCell.CELL_TYPE_STRING:
+                        sb.append(cell.getStringCellValue());
+                        break;
+                    case HSSFCell.CELL_TYPE_NUMERIC:
+                        sb.append(String.valueOf(cell.getNumericCellValue()));
+                        break;
+                    case HSSFCell.CELL_TYPE_BOOLEAN:
+                        sb.append(String.valueOf(cell.getBooleanCellValue()));
+                        break;
+                    case HSSFCell.CELL_TYPE_BLANK:
+                        sb.append("--");
+                }
+                sb.append("</td>");
+            }
+            sb.append("</tr></table>");
         }
-        catch (Exception e) {
-        
+        catch (Exception e)
+        {
+            e.printStackTrace();
         }
+        System.out.println(sb.toString());
         return sb.toString();
     }
-
-
+    
     public String analyseExcel(HttpServletRequest request, HttpServletResponse response)
     {
         HttpSession session = request.getSession(true);
@@ -257,9 +346,9 @@ public class AnalyseAction
         File exportFile = new File(request.getSession().getServletContext().getRealPath("/Compare/" + exportFileName));
         File importFile = new File(request.getSession().getServletContext().getRealPath("/Compare/" + importFileName));
         
-        // 读取excel 文件 存入集合中        
-        session.setAttribute("exSheet", findSheets(exportFileName,exportFile));
-        session.setAttribute("imSheet", findSheets(importFileName,importFile));
+        // 读取excel 文件 存入集合中
+        session.setAttribute("exSheet", findSheets(exportFileName, exportFile));
+        session.setAttribute("imSheet", findSheets(importFileName, importFile));
         session.setAttribute("exFilenName", exportFileName);
         session.setAttribute("imFilenName", importFileName);
         return "success";
@@ -267,7 +356,7 @@ public class AnalyseAction
     
     private List<String> findSheets(String fileName, File file)
     {
-        List<String> lstSheet= null;
+        List<String> lstSheet = null;
         // 判断是不是excel 2007
         boolean isE2007 = false;
         if (fileName.endsWith("xlsx"))
@@ -287,15 +376,15 @@ public class AnalyseAction
             {
                 wb = new HSSFWorkbook(is);
             }
-            //获得Sheet的个数
+            // 获得Sheet的个数
             int numberOfSheet = wb.getNumberOfSheets();
             
             lstSheet = new ArrayList<String>();
-            //把Sheet的名字加入集合中
-            for(int i = 0; i<numberOfSheet;i++)
+            // 把Sheet的名字加入集合中
+            for (int i = 0; i < numberOfSheet; i++)
             {
                 lstSheet.add(wb.getSheetAt(i).getSheetName());
-            }      
+            }
         }
         catch (Exception e)
         {
